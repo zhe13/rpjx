5871a57800afbbc58dbd4ce805c54c2cdcc69988
zhe13 <lovewutianzhe@vip.qq.com>
2015-06-04 11:23:31 +0800

    屏幕滚动
    一些有的没的的修改

--- Parse/js/app_1.js
+++ Parse/js/app_1.js
@@ -4,111 +4,117 @@
 //name 	:	Parse_Talk2U_SaveTest
 //
 var ParseApplicationID = "DPwW3VlJOJn7Cz2H2H1uJOCzA6PUrYHPmzMRUz2u";
-var ParseRESTKey = "oB6ABoyY7m7Jb7vsY0i6pjH3ScjTgRiUYlDzGQvz";
+var ParseRESTKey	   = "oB6ABoyY7m7Jb7vsY0i6pjH3ScjTgRiUYlDzGQvz";
 
-$(document).ready(function() {
-    setInterval(function() {
-      getMessage();
-      console.log('can？');
-    }, 2000);
+$(document).ready(function(){
 
-    $("#send").click(function() {
-      var username = $('input[name=username]').val();
-      var message = $('input[name=message]').val();
-      console.log(username);
-      console.log('!');
-      //当提交消息时，ajax发送了一个POST请求
-      $.ajax({
-        url: 'https://api.parse.com/1/classes/MessageBoard',
-        headers: {
-          'X-Parse-Application-Id': ParseApplicationID,
-          'X-Parse-REST-API-Key': ParseRESTKey
+	setInterval(function(){
+		getMessage();
+		console.log('can？');
+	},2000);
 
-        },
-        contentType: 'application/json',
-        dataType: 'json',
-        processData: false,
-        data: JSON.stringify({
-          'username': username,
-          'message': message
-        }),
-        type: 'POST',
-        success: function() {
-          console.log('sent');
-          getMessage();
-        },
-        error: function() {
-          console.log('error');
-        }
-      });
-    });
+	$("#send").click(function(){
+		var username = $('input[name=username]').val();
+		var message  = $('input[name=message]').val();
+		console.log(username);
+		console.log('!');
+		//当提交消息时，ajax发送了一个POST请求
+		$.ajax({
+			url : 'https://api.parse.com/1/classes/MessageBoard',
+			headers : {
+				'X-Parse-Application-Id' : ParseApplicationID,
+				'X-Parse-REST-API-Key'	 : ParseRESTKey
 
-    $("#delete").click(function() {
-      var objectid = $('input[name=objectId]').val();
-      console.log('Delete!');
-      console.log(objectid);
-      $.ajax({
-        url: 'https://api.parse.com/1/classes/MessageBoard/' +
-          objectid,
-        headers: {
-          'X-Parse-Application-Id': ParseApplicationID,
-          'X-Parse-REST-API-Key': ParseRESTKey
+			},
+			contentType : 'application/json',
+			dataType	: 'json',
+			processData : false,
+			data 		: JSON.stringify({
+				'username': username,
+				'message' : message
+			}),
+			type 		: 'POST',
+			success		: function(){
+				console.log('sent');
+				getMessage();
+				//scrollBottom();
+			},
+			error 		: function(){
+				console.log('error');
+			}
+		});	 
+	});
 
-        },
-        contentType: 'application/json',
-        dataType: 'json',
-        data: JSON.stringify({
+	$("#delete").click(function(){
+		var objectid  = $('input[name=objectId]').val();
+		console.log('Delete!');
+		console.log(objectid);
+		$.ajax({
+			url : 'https://api.parse.com/1/classes/MessageBoard/'+objectid,
+			headers : {
+				'X-Parse-Application-Id' : ParseApplicationID,
+				'X-Parse-REST-API-Key'	 : ParseRESTKey
 
-        }),
-        type: 'DELETE',
-        success: function() {
-          getMessage();
-        },
-        error: function() {
-          console.log('error');
-        }
-      });
-    });
-  })
-  //使用同样的ajax方法获取远程REST API的消息列表
-function getMessage() {
-    $.ajax({
-      url: 'https://api.parse.com/1/classes/MessageBoard',
-      headers: {
-        'X-Parse-Application-Id': ParseApplicationID,
-        'X-Parse-REST-API-Key': ParseRESTKey
-      },
-      contentType: 'application/json',
-      dataType: 'json',
-      type: 'GET',
-      //如果请求成功（返回200OK的消息）调用updateView
-      success: function(data) {
-        console.log('get');
-        var tempData = data;
-        updateView(tempData);
-      },
-      error: function() {
-        console.log('error');
-      },
-      complete: function(XHR, TS) {
-        XHR = null
-          //console.log('xhr is deleted');
-      }
-    });
-  }
-  //显示得到的消息
-function updateView(message) {
-  var table = $('.table tbody'); //使用选择器选中元素并且创建一个引用
-  table.html(''); //清空这个元素的inner HTML
-  //使用jQuery.each便利消息
-  $.each(message.results, function(index, value) {
-    var trEl =
-      $('<tr><td>' + value.username + '</td><td>' + value.message +
-        '</td><td>' + value.objectId + '</td></tr>');
-    //追加表格
-    table.append(trEl);
-  });
-  console.log(message);
-  //滚动到最底端
-  $("#Scroll").scrollTop($("#Scroll")[0].scrollHeight);
+			},
+			contentType : 'application/json',
+			dataType	: 'json',
+			data 		: JSON.stringify({
+				
+			}),
+			type 		: 'DELETE',
+			success 	: function(){
+				getMessage();
+			},
+			error 		: function(){
+				console.log('error');
+			}
+		});
+	});
+})
+//使用同样的ajax方法获取远程REST API的消息列表
+function getMessage(){
+	$.ajax({
+		url : 'https://api.parse.com/1/classes/MessageBoard',
+		headers:{
+			'X-Parse-Application-Id' : ParseApplicationID,
+			'X-Parse-REST-API-Key'	 : ParseRESTKey
+		},
+		contentType : 'application/json',
+		dataType 	: 'json',
+		type 		: 'GET',
+		//如果请求成功（返回200OK的消息）调用updateView
+		success 	: function(data){
+			console.log('get');
+			updateView(data);
+		},
+		error 		: function(){
+			console.log('error');
+		}
+	});
+}
+//显示得到的消息
+function updateView(message){
+	var table=$('.table tbody');//使用选择器选中元素并且创建一个引用
+	table.html('');//清空这个元素的inner HTML
+	//使用jQuery.each便利消息
+	$.each(message.results,function (index,value){
+		var trEl = 
+		$('<tr><td>'
+			+ value.username
+			+ '</td><td>'
+			+ value.message
+			+ '</td><td>'
+			+ value.objectId
+			+ '</td></tr>');
+		//追加表格
+		table.append(trEl);
+	});
+	console.log(message);
+
+	scrollBottom();
+}
+//自动卷动到底部
+function scrollBottom(){
+	var div = document.getElementById('ChatPanel');
+	div.scrollTop = div.scrollHeight; 
 }
